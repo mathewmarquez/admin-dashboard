dfabb7b97b9259cdc1d77c7d0f8e39fabbcaf552
Mathew Marquez <mmarquez@Macbook-Air-RG.local>
2023-11-25 20:47:47 -0600

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'HTML | CSS Tutorial',
+        productNumber: '2121',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Portfolio | ProjectHUB',
+        productNumber: '1738',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Interview | Resume Guide',
+        productNumber: '2319',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
